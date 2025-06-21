620872378bf1fcb22ccf3b7657a1d1395c72f80c
Kyo <kyo@Kyos-MacBook-Pro.local>
2025-06-22 00:13:50 +0800

    chore(tooling): setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // 禁止使用 any
+    '@typescript-eslint/no-explicit-any': 'error',
+    // Prettier 冲突交由 plugin:prettier 处理
+  },
+  ignorePatterns: ['dist', 'build', 'node_modules'],
+}; 
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 ### Added
 - docs/DEVELOPMENT.md（本地开发指南）
+- ESLint 与 Prettier 配置文件（.eslintrc.js, .prettierrc）
 
 ### Changed 
